29a1f36c5ef18c34d78323b7b7d2574218b9e3b1
kelly_maass <kmaass77627@gmail.com>
2024-05-25 19:07:15 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Target the paragraph element with id="text"
+   
     const textElement = document.getElementById("text");
-  
-    // Replace the text content using textContent
     textElement.textContent = "This is really cool!";
   });
   
